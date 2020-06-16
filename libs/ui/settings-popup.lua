949885ddcf973c40f5d0143f273e57f62d3d17f9
Danny Glover <revolt64@outlook.com>
2020-06-16 22:21:18 +0100

    Settings popup: added preferences categories
    - General, audio, music list, podcasts etc.

--- libs/ui/settings-popup.lua
+++ libs/ui/settings-popup.lua
@@ -12,13 +12,12 @@ local filledButtonLib = require("libs.ui.filled-button")
 local alertPopupLib = require("libs.ui.alert-popup")
 local ratings = require("libs.ui.ratings")
 local activityIndicator = require("libs.ui.activity-indicator")
+local common = require("libs.ui.media-bar.common")
 local mMin = math.min
 local mMax = math.max
 local sFormat = string.format
 local sSub = string.sub
 local uPack = unpack
-local titleFont = "fonts/Jost-500-Medium.ttf"
-local subTitleFont = "fonts/Jost-400-Book.ttf"
 local maxDisplayWidth = 1024
 local maxDisplayHeight = 768
 local group = display.newGroup()
@@ -29,6 +28,11 @@ local userHomeDirectoryPath = isWindows and "%HOMEPATH%\\" or os.getenv("HOME")
 local documentsPath = system.pathForFile(nil, system.DocumentsDirectory)
 local created = false
 
+local function showHandCursor()
+	native.setProperty("mouseCursor", "pointingHand")
+	return true
+end
+
 function M.create()
 	if (created) then
 		return group
@@ -37,7 +41,7 @@ function M.create()
 	local interactionDisableRect = nil
 	local background = nil
 	local titleText = nil
-	local separatorLine = nil
+	local separatorLineTitle = nil
 	local buttonGroup = nil
 	local cancelButton = nil
 	local confirmButton = nil
@@ -108,13 +112,20 @@ function M.create()
 				return true
 			end
 		)
+		background:addEventListener(
+			"mouse",
+			function(event)
+				native.setProperty("mouseCursor", "arrow")
+				return true
+			end
+		)
 		self:insert(background)
 
 		titleText =
 			display.newText(
 			{
 				text = "Preferences",
-				font = titleFont,
+				font = common.titleFont,
 				x = 0,
 				y = 0,
 				fontSize = maxHeight * 0.04,
@@ -127,11 +138,117 @@ function M.create()
 		titleText:setFillColor(uPack(theme:get().textColor.primary))
 		self:insert(titleText)
 
-		separatorLine = display.newRoundedRect(0, 0, background.contentWidth - 40, 1, 1)
-		separatorLine.x = background.x
-		separatorLine.y = titleText.y + titleText.contentHeight * 0.5
-		separatorLine:setFillColor(uPack(theme:get().textColor.secondary))
-		self:insert(separatorLine)
+		separatorLineTitle = display.newRoundedRect(0, 0, background.contentWidth - 40, 1, 1)
+		separatorLineTitle.x = background.x
+		separatorLineTitle.y = titleText.y + titleText.contentHeight * 0.5
+		separatorLineTitle:setFillColor(uPack(theme:get().textColor.secondary))
+		self:insert(separatorLineTitle)
+
+		local separatorLineCategories = display.newRoundedRect(0, 0, 1, background.contentHeight - 64, 1)
+		separatorLineCategories.anchorY = 0
+		separatorLineCategories.x = background.x - (background.contentWidth / 4)
+		separatorLineCategories.y = separatorLineTitle.y + separatorLineTitle.contentHeight + 5
+		separatorLineCategories:setFillColor(uPack(theme:get().textColor.secondary))
+		self:insert(separatorLineCategories)
+
+		-- preference categories
+		local generalPreferences =
+			buttonLib.new(
+			{
+				iconName = "General",
+				font = common.titleFont,
+				fontSize = maxHeight * 0.025,
+				parent = self,
+				onClick = function(event)
+				end
+			}
+		)
+		generalPreferences.anchorX = 0
+		generalPreferences.x = separatorLineTitle.x - separatorLineTitle.contentWidth * 0.5 + 10
+		generalPreferences.y = separatorLineTitle.y + separatorLineTitle.contentHeight + generalPreferences.contentHeight
+		generalPreferences:addEventListener("mouse", showHandCursor)
+
+		local audioPreferences =
+			buttonLib.new(
+			{
+				iconName = "Audio",
+				font = common.titleFont,
+				fontSize = maxHeight * 0.025,
+				parent = self,
+				onClick = function(event)
+				end
+			}
+		)
+		audioPreferences.anchorX = 0
+		audioPreferences.x = generalPreferences.x
+		audioPreferences.y = generalPreferences.y + generalPreferences.contentHeight + audioPreferences.contentHeight * 0.5
+		audioPreferences:addEventListener("mouse", showHandCursor)
+
+		local musicListPreferences =
+			buttonLib.new(
+			{
+				iconName = "Music List",
+				font = common.titleFont,
+				fontSize = maxHeight * 0.025,
+				parent = self,
+				onClick = function(event)
+				end
+			}
+		)
+		musicListPreferences.anchorX = 0
+		musicListPreferences.x = generalPreferences.x
+		musicListPreferences.y =
+			audioPreferences.y + audioPreferences.contentHeight + musicListPreferences.contentHeight * 0.5
+		musicListPreferences:addEventListener("mouse", showHandCursor)
+
+		local playlistPreferences =
+			buttonLib.new(
+			{
+				iconName = "Playlists",
+				font = common.titleFont,
+				fontSize = maxHeight * 0.025,
+				parent = self,
+				onClick = function(event)
+				end
+			}
+		)
+		playlistPreferences.anchorX = 0
+		playlistPreferences.x = generalPreferences.x
+		playlistPreferences.y =
+			musicListPreferences.y + musicListPreferences.contentHeight + playlistPreferences.contentHeight * 0.5
+		playlistPreferences:addEventListener("mouse", showHandCursor)
+
+		local radioPreferences =
+			buttonLib.new(
+			{
+				iconName = "Radio",
+				font = common.titleFont,
+				fontSize = maxHeight * 0.025,
+				parent = self,
+				onClick = function(event)
+				end
+			}
+		)
+		radioPreferences.anchorX = 0
+		radioPreferences.x = generalPreferences.x
+		radioPreferences.y = playlistPreferences.y + playlistPreferences.contentHeight + radioPreferences.contentHeight * 0.5
+		radioPreferences:addEventListener("mouse", showHandCursor)
+
+		local podcastPreferences =
+			buttonLib.new(
+			{
+				iconName = "Podcasts",
+				font = common.titleFont,
+				fontSize = maxHeight * 0.025,
+				parent = self,
+				onClick = function(event)
+				end
+			}
+		)
+		podcastPreferences.anchorX = 0
+		podcastPreferences.x = generalPreferences.x
+		podcastPreferences.y = radioPreferences.y + radioPreferences.contentHeight + podcastPreferences.contentHeight * 0.5
+		podcastPreferences:addEventListener("mouse", showHandCursor)
 
 		buttonGroup = display.newGroup()
 
@@ -166,7 +283,7 @@ function M.create()
 
 		buttonGroup.anchorChildren = true
 		buttonGroup.anchorX = 0.5
-		buttonGroup.x = background.x
+		buttonGroup.x = background.x + (background.contentWidth / 8)
 		buttonGroup.y = background.y + background.contentHeight * 0.5 - buttonGroup.contentHeight
 
 		self.isVisible = true
